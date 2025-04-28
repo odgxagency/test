ac8ae4a5d81965c403e0d93d4bc3c50c16718960
uxffm <35499917+uxffm@users.noreply.github.com>
2025-04-28 12:21:43 +0200

    yes

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,5 @@
 
 export default new Map([
-["src/content/pages/english/elements.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fenglish%2Felements.mdx&astroContentModuleFlag=true")],
-["src/content/pages/german/elements.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fgerman%2Felements.mdx&astroContentModuleFlag=true")]]);
+["src/content/pages/german/elements.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fgerman%2Felements.mdx&astroContentModuleFlag=true")],
+["src/content/pages/english/elements.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fenglish%2Felements.mdx&astroContentModuleFlag=true")]]);
 		
